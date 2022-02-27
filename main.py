0477420b7f1dc7379e2f4eaef188a7e1590b3385
Dazai-Os <vysokolyan2016@gmail.com>
2022-02-27 18:49:26 +0300

    add screen

--- main.py
+++ main.py
@@ -48,10 +48,10 @@ class TicTac():
             rand_check = False
             while rand_check == False:
                 if price_cell == 2:
-                    future_win = random.choice([0, 2, 6, 8, 1, 3, 5, 7])
+                    future_win = random.choice([0, 2, 6, 8])
                     if self.board[future_win] == " ":
                         rand_check = True
-                elif price_cell == 0:
+                elif price_cell == 1:
                     future_win = random.choice([1, 3, 5, 7])
                     if self.board[future_win] == " ":
                         rand_check = True
